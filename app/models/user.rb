6e70b3eea4c68de39eae135ffb4ef3c7aec897e8
lukeymac <lukey.mac@outlook.com>
2017-06-19 19:05:21 -0400

    added create_current_cart and remove_cart methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :carts
+  belongs_to :current_cart, :cass_name => "Cart"
+
+  def create_current_cart
+    new_cart = carts.create
+    self.current_cart_id = new_card.id
+    save
+  end
+
+  def remove_cart
+    self.current_cart_id = nil
+    save
+  end
+  
 end
